e2a498a99d38ae2f1f473504ba7fed8bb4f70f9d
Danny Yan <yan4646@hotmail.com>
2013-12-29 20:57:34 -0500

    Removed unnecessary additional check involving prime numbers

--- pe.py
+++ pe.py
@@ -8,7 +8,7 @@ def IsPrime(number):
 		return True
 	if not number & 1:
 		return False
-	for Counter in range(3, int(number**0.5)+2, 2):
+	for Counter in range(3, int(number**0.5)+1, 2):
 		if number % Counter == 0:
 			return False
 	return True
@@ -54,6 +54,8 @@ def GeneratePrimes(size):
 		Counter += 2;
 	return PrimeList;
 
+def NumberOfDivisors(number):
+	return
 
 def ReverseString(string):
 	return string[::-1]
@@ -61,7 +63,7 @@ def ReverseString(string):
 #	ID	1
 #	Multiples of 3 and 5
 #	Find the sum of all the multiples of 3 or 5
-def MultiplesOf3And5(number):
+def MultiplesOf3And5(number=1000):
 	result = 0
 	for Counter in range(0,number,3):
 		result += Counter
@@ -73,7 +75,7 @@ def MultiplesOf3And5(number):
 #	ID	2
 #	Even Fibonacci numbers
 #	Find the sum of the even-valued terms of the Fibonacci sequence whose values does not exceed four million
-def EvenFibonacciNumbers(number):
+def EvenFibonacciNumbers(number=4000000):
 	result = 0
 	x1 = 1
 	x2 = 2
@@ -87,11 +89,11 @@ def EvenFibonacciNumbers(number):
 #	ID	3
 #	Largest prime factor
 #	Return the largest prime factor
-def LargestPrimeFactor(number):
+def LargestPrimeFactor(number=600851475143):
 	if IsPrime(number):
 		return number
 	result = 2
-	for Counter in range(3, int(number**0.5)+2, 2):
+	for Counter in range(3, int(number**0.5)+1, 2):
 		if IsPrime(Counter) and number % Counter == 0:
 			result = Counter
 	return result
@@ -193,7 +195,7 @@ def SummationOfPrimes(number=2000000):
 	Result = 0
 	PrimeList = list(range(3,number,2))
 	SquaredNumberIndex = 0
-	SquaredNumber = int(number**0.5) + 2
+	SquaredNumber = int(number**0.5) + 1
 	for Counter in range(len(PrimeList)):
 		if PrimeList[Counter] > SquaredNumber:
 			SquaredNumberIndex = Counter
@@ -203,34 +205,39 @@ def SummationOfPrimes(number=2000000):
 		Index2 = Index + 1
 		while Index2 < Length:
 			if PrimeList[Index2]%PrimeList[Index] == 0:
-				print str(PrimeList[Index]) + " " + str(PrimeList[Index2])
 				PrimeList.pop(Index2)
 				Length -= 1
 				Index2 -= 1
 			Index2 += 1
 	for Prime in PrimeList:
 		Result += Prime
-	print PrimeList
 	return Result + 2
 
 #	ID 	11
 #	Largest product in a grid
 #	Find the greatest product of four adjacent numbers in the any direction in the 20x20 grid
-def LargestProductInAGrid(adjacentNumbers, number=[[8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],[49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0],[81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65],[52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91],[22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],[24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],[32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],[67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21],[24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],[21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95],[78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92],[16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57],[86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],[19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40],[04, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],[88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],[04, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36],[20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16],[20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],[01, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]]):
+def LargestProductInAGrid(adjacentNumbers, grid=[[8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],[49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0],[81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65],[52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91],[22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],[24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],[32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],[67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21],[24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],[21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95],[78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92],[16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57],[86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],[19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40],[04, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],[88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],[04, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36],[20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16],[20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],[01, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]]):
 	Result = 0
 	for CoordinateX in range(0,20):
 		for CoordinateY in range(0,20):
 			ProductA = ProductB = ProductC = ProductD = 1
 			for Next in range(0,adjacentNumbers):
 				if CoordinateX + Next < 20:
-					ProductA *= number[CoordinateX+Next][CoordinateY]
+					ProductA *= grid[CoordinateX+Next][CoordinateY]
 				if max(CoordinateX,CoordinateY) + Next < 20:
-					ProductB *= number[CoordinateX+Next][CoordinateY+Next]
+					ProductB *= grid[CoordinateX+Next][CoordinateY+Next]
 				if CoordinateY + Next < 20:
-					ProductC *= number[CoordinateX][CoordinateY+Next]
+					ProductC *= grid[CoordinateX][CoordinateY+Next]
 				if CoordinateX + Next < 20 and CoordinateY - Next >= 0:
-					ProductD *= number[CoordinateX+Next][CoordinateY-Next]
+					ProductD *= grid[CoordinateX+Next][CoordinateY-Next]
 			MaxProduct = max(ProductA, ProductB, ProductC, ProductD)
 			if MaxProduct > Result:
 				Result = MaxProduct
 	return Result
+
+#	ID 	12
+#	Highly divisible triangular number
+#	Find the value of the first triangle number to have over five hundred divisors
+def HighlyDivisibleTriangularNumber(divisors=500):
+
+	return
